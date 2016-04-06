0b136263638717a0061260f0ea5cd888ba97e6b0
Damien Maillard <dmaillard06@gmail.com>
2016-04-06 11:28:58 +0200

    eslint + make thenable

--- index.js
+++ index.js
@@ -1,52 +1,64 @@
 var Timeout = {
-	timer: null,
+    timer: null,
 
-	constructor: function(fn, bind, value){
-		this.fn = fn;
-		this.bind = bind;
-		if( arguments.length > 2 ){
-			this.set(value);
-		}
-	},
+    constructor: function(fn, bind, value) {
+        this.fn = fn;
+        this.bind = bind;
+        if (arguments.length > 2) {
+            this.set(value);
+        }
+        this.promise = new Promise(function(resolve) {
+            this.resolve = resolve;
+        }.bind(this));
+    },
 
-	valueOf: function(){
-		return this.value;
-	},
+    valueOf: function() {
+        return this.value;
+    },
 
-	create: function(){
-		var timeout = Object.create(this);
+    create: function() {
+        var timeout = Object.create(this);
 
-		timeout.constructor.apply(timeout, arguments);
+        timeout.constructor.apply(timeout, arguments);
 
-		return timeout;
-	},
+        return timeout;
+    },
 
-	expire: function(){
-		this.fn.call(this.bind);
-	},
+    expire: function() {
+        this.resolve();
+        this.fn.call(this.bind);
+    },
 
-	has: function(){
-		return this.timer !== null;
-	},
+    has: function() {
+        return this.timer !== null;
+    },
 
-	get: function(){
-		return this.value;
-	},
+    get: function() {
+        return this.value;
+    },
 
-	set: function(value){
-		this.delete();
-		this.value = value;
-		if( value ){
-			this.timer = setTimeout(this.expire.bind(this), value);
-		}
-	},
+    set: function(value) {
+        this.delete();
+        this.value = value;
+        if (value) {
+            this.timer = setTimeout(this.expire.bind(this), value);
+        }
+    },
 
-	delete: function(){
-		if( this.timer ){
-			clearTimeout(this.timer);
-			this.timer = null;
-		}
-	}
+    delete: function() {
+        if (this.timer) {
+            clearTimeout(this.timer);
+            this.timer = null;
+        }
+    },
+
+    then: function(onResolve, onReject) {
+        return this.promise.then(onResolve, onReject);
+    },
+
+    catch: function(onReject) {
+        return this.then(null, onReject);
+    }
 };
 
 export default Timeout;
